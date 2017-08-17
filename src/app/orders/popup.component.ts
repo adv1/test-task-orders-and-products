1757080a446696d2e7b0ba3a788f7500003b1f4d
adv1 <andrewlozov@gmail.com>
2017-08-17 13:47:07 +0300

    angular

--- src/app/orders/popup.component.ts
+++ src/app/orders/popup.component.ts
@@ -1,36 +1,35 @@
 import { Component, Input } from '@angular/core';
-
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'ngbd-modal-content',
   template: `
     <div class="modal-header">
-      <p class="modal-title" style="text-align: center; font-weight: 600">Вы уверены, что хотите удалить этот приход?</p>
+      <p class="modal-title">Вы уверены, что хотите удалить этот приход?</p>
       <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
         <span aria-hidden="true">&times;</span>
       </button>
     </div>
     <div class="modal-body">
-      <div style="display: flex; justify-content: space-around">
+      <div class="options-content">
         <div>
-          <h1 style="text-align: start">.</h1>
+          <h1>.</h1>
         </div>
         <div>
           <img src="./assets/images/monitor.jpg"/>
         </div>
-        <div style="display: flex; flex-direction: column">
+        <div id="title-current-order">
           <div id="current-title">Длинное название прихода</div>
-          <div id="current-serial-number" style="color: lightslategray">1234</div>
+          <div id="current-serial-number">1234</div>
         </div>
-
       </div>
     </div>
-    <div class="modal-footer" style="background-color: #8BC34A; text-decoration-color: white">
-      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Отменить</button>
-      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Удалить</button>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-light" (click)="activeModal.close('Close click')">Отменить</button>
+      <button type="button" class="btn btn-light" (click)="activeModal.close('Close click')">Удалить</button>
     </div>
-  `
+  `,
+  styleUrls: [ './popup.component.scss']
 })
 export class NgbdModalContentComponent {
   @Input() name;
